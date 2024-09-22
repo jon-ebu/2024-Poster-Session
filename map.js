937a41965af45793278c0df584871fe60c4083b2
jonebu <ebueng23@gmail.com>
2024-09-21 17:26:10 -0700

    Updating map boundaries

--- map.js
+++ map.js
@@ -1,5 +1,15 @@
+// Define the geographical bounds
+var bounds = [
+    [34.1055463, -117.7123690], // Southwest corner
+    [34.1066834, -117.71166090]  // Northeast corner
+];
+
+
 // Initialize map and add a layer using Google Satellite view
 var map = L.map('map').setView([34.106109928727946, -117.7120061219193], 20);
+
+// Set the max bounds for the map
+map.setMaxBounds(bounds);
 
 var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
     maxZoom: 25,
