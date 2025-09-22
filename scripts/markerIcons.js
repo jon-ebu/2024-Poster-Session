7f641fd811eeb9c5e195ddc901afb00df9b73562
jon-ebu <ebueng23@gmail.com>
2025-09-22 16:07:46 -0700

    Updating color key

--- scripts/markerIcons.js
+++ scripts/markerIcons.js
@@ -8,55 +8,83 @@ function getColorByEaselBoardId(value) {
   let color;
   switch (easelBoardId) {
     case "B":
-      color = "#E69F00"; // yellow-orange
-      break;
-    case "BC":
-      color = "#56B4E9"; // light blue
+      color = "#008000"; // green (B-1 through B-7)
       break;
     case "BCS":
-      color = "#009E73"; // teal
+      color = "#FFA500"; // orange (BCS-1, BCS-2, BCS-3)
       break;
-    case "BE":
-      color = "#F0E442"; // light yellow
+    case "BHC":
+      color = "#008000"; // green (Biology, Hixon Center - follows Biology color)
       break;
     case "C":
-      color = "#0072B2"; // dark blue
+      color = "#FF0000"; // red (C-1 through C-17)
       break;
     case "CEP":
-      color = "#D55E00"; // orange
+      color = "#FFA500"; // orange (CEP1 & CEP2)
       break;
     case "CHC":
-      color = "#CC79A7"; // pink
+      color = "#FFA500"; // orange (CHC1, CHC2, CHC3)
+      break;
+    case "CP":
+      color = "#FFA500"; // orange (CP1)
       break;
     case "CS":
-      color = "#FF69B4"; // hot pink
+      color = "#87CEEB"; // light blue (CS-1 through CS-22)
+      break;
+    case "CSE":
+      color = "#FFA500"; // orange (CSE-1, CSE-2)
+      break;
+    case "CSEM":
+      color = "#FFA500"; // orange (CSEM-1)
+      break;
+    case "CSEP":
+      color = "#FFA500"; // orange (CSEP-1, CSEP-2)
       break;
     case "CSHC":
-      color = "#E69F00"; // yellow-orange
+      color = "#FFA500"; // orange (CSHC-1, CSHC-2)
       break;
     case "CSM":
-      color = "#56B4E9"; // light blue
-      break;
-    case "CSN":
-      color = "#009E73"; // teal
+      color = "#FFA500"; // orange (CSM-1)
       break;
     case "E":
-      color = "#0072B2"; // dark blue
+      color = "#000000"; // black (E-1 through E-26)
       break;
-    case "EM":
-      color = "#FF4500"; // orange-red
+    case "EP":
+      color = "#FFA500"; // orange (EP-1)
+      break;
+    case "HC":
+      color = "#FFD700"; // gold (HC-1)
+      break;
+    case "HSA":
+      color = "#008080"; // teal (HSA-1)
+      break;
+    case "HSAM":
+      color = "#FFA500"; // orange (HSAM-1)
       break;
     case "M":
-      color = "#D55E00"; // orange
-      break;
-    case "O":
-      color = "#CC79A7"; // pink
+      color = "#FFA500"; // orange (M-1 through M-5)
       break;
     case "P":
-      color = "#FFD700"; // gold
+      color = "#800080"; // purple (P-1 through P-13)
+      break;
+    // Legacy cases for backward compatibility
+    case "BC":
+      color = "#56B4E9"; // light blue (legacy)
+      break;
+    case "BE":
+      color = "#F0E442"; // light yellow (legacy)
+      break;
+    case "CSN":
+      color = "#009E73"; // teal (legacy)
+      break;
+    case "EM":
+      color = "#FF4500"; // orange-red (legacy)
+      break;
+    case "O":
+      color = "#CC79A7"; // pink (legacy)
       break;
     case "SSEF":
-      color = "#E69F00"; // yellow-orange
+      color = "#E69F00"; // yellow-orange (legacy)
       break;
     default:
       color = "#000000"; // black for unrecognized easelBoardId
